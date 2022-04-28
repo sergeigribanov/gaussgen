765ad82d8806fc6616edf5653be41f317a1c50de
Sergei Gribanov <ssgribanov@gmail.com>
2022-04-28 15:20:11 +0700

    a number of changes

--- library/include/gaussgen.hpp
+++ library/include/gaussgen.hpp
@@ -5,5 +5,7 @@
 namespace gaussgen {
   Eigen::VectorXd gaussgen(const Eigen::VectorXd&, const Eigen::MatrixXd&,
                            TRandom*);
+  Eigen::VectorXd gaussgen_nfirst(std::size_t, const Eigen::VectorXd&, const Eigen::MatrixXd&,
+                           TRandom*);
   Eigen::MatrixXd inverse(const Eigen::MatrixXd&);
 }
--- library/src/gaussgen.cpp
+++ library/src/gaussgen.cpp
@@ -14,6 +14,25 @@ Eigen::VectorXd gaussgen::gaussgen(const Eigen::VectorXd& x,
   return tm * result;
 }
 
+Eigen::VectorXd gaussgen::gaussgen_nfirst(std::size_t n,
+                                          const Eigen::VectorXd& x,
+                                          const Eigen::MatrixXd& incov,
+                                          TRandom* rnd) {
+  Eigen::MatrixXd invcov_block = incov.block(0, 0, n, n);
+  Eigen::EigenSolver<Eigen::MatrixXd> solver(invcov_block);
+  Eigen::VectorXd sigma = solver.eigenvalues().real().array().pow(-0.5);
+  Eigen::MatrixXd tm = solver.eigenvectors().real();
+  Eigen::VectorXd mean = tm.partialPivLu().solve(x.head(n));
+  Eigen::VectorXd nfirst = Eigen::VectorXd::Zero(mean.size());
+  for (long i = 0; i < mean.size(); ++i) {
+    nfirst(i) = rnd->Gaus(mean(i), sigma(i));
+  }
+  Eigen::VectorXd result(x.size());
+  result.segment(0, n) = tm * nfirst;
+  result.tail(x.size() - n) = x.tail(x.size() - n);
+  return result;
+}
+
 Eigen::MatrixXd gaussgen::inverse(const Eigen::MatrixXd& mx) {
   return mx.inverse();
 }
